1eb93d2f9a260e645b9b30300110c56429dbd495
Filip Strajnar <filip.strajnar@gmail.com>
2024-09-30 21:40:23 +0200

    Enabled selection of cipher mode (defaults to CBC), added method to conveniently create AES SymmetricCryptoStream.

--- Proculite.Common/Security/Cryptography/SimpleAes.cs
+++ Proculite.Common/Security/Cryptography/SimpleAes.cs
@@ -4,13 +4,24 @@ namespace Proculite.Common.Security.Cryptography
 {
     public static class SimpleAes
     {
-        public static Aes Create(byte[] key, byte[] iv)
+        public static Aes Create(byte[] key, byte[] iv, CipherMode cipherMode = CipherMode.CBC)
         {
             Aes aes = Aes.Create();
             aes.IV = iv;
             aes.Key = key;
+            aes.Mode = cipherMode;
 
             return aes;
+        }
+
+        public static SymmetricCryptoStream CreateCryptoStream(
+            byte[] key,
+            byte[] iv,
+            CipherMode cipherMode = CipherMode.CBC
+        )
+        {
+            Aes aes = Create(key, iv, cipherMode);
+            return new SymmetricCryptoStream(aes);
         }
     }
 }
